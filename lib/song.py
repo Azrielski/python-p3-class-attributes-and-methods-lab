97d83f8512d6e8a07e39e4a2b50dc922732e1a43
Azrielski <victorngae20@gmail.com>
2025-02-23 00:05:12 +0300

    final changes

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,60 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}  
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        
+        Song.add_song_to_count()
+        Song.add_to_artists(artist)
+        Song.add_to_genres(genre)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1 
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+
+
+song1 = Song("99 Problems", "Jay-Z", "Rap")
+song2 = Song("Halo", "Beyonce", "Pop")
+song3 = Song("In the End", "Linkin Park", "Rock")
+song4 = Song("Run the World", "Beyonce", "Pop")
+
+
+print("Total Songs Count:", Song.count)
+print("Artists:", Song.artists)
+print("Genres:", Song.genres)
+print("Genre Count:", Song.genre_count)
+print("Artist Count:", Song.artist_count)
